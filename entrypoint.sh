ac7df7fabfa455cb56f4de36eb5975aeefdfc81c
abingcbc <19990626.love@163.com>
2021-02-18 22:10:44 +0800

    custom

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+mkdir -p /root/.ssh/
+echo "${DEPLOY_KEY}" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+
+# setup deploy git account
+git config --global user.name "${USER_NAME}"
+git config --global user.email "${USER_EMAIL}"
+
 # check values
 if [ -n "${PUBLISH_REPOSITORY}" ]; then
     PRO_REPOSITORY=${PUBLISH_REPOSITORY}
@@ -9,35 +18,10 @@ else
     PRO_REPOSITORY=${GITHUB_REPOSITORY}
 fi
 
-if [ -z "$PUBLISH_DIR" ]
-then
-  echo "You must provide the action with the folder path in the repository where your compiled page generate at, example public."
-  exit 1
-fi
-
-if [ -z "$BRANCH" ]
-then
-  echo "You must provide the action with a branch name it should deploy to, for example master."
-  exit 1
-fi
-
-if [ -z "$PERSONAL_TOKEN" ]
-then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
-  exit 1
-fi
-
-REPOSITORY_PATH="https://x-access-token:${PERSONAL_TOKEN}@github.com/${PRO_REPOSITORY}.git"
-
-# deploy to 
-echo "Deploy to ${PRO_REPOSITORY}"
-
-# Directs the action to the the Github workspace.
-cd $GITHUB_WORKSPACE 
+ls
 
 echo "npm install ..." 
 npm install
-
 
 echo "Clean folder ..."
 ./node_modules/hexo/bin/hexo clean
@@ -45,30 +29,20 @@ echo "Clean folder ..."
 echo "Generate file ..."
 ./node_modules/hexo/bin/hexo generate
 
+mv public /root/public
+
+git checkout "${BRANCH}"
+
 echo "copy CNAME if exists"
-if [ -n "${CNAME}" ]; then
-    echo $CNAME > public
+if [ -n "CNAME" ]; then
+  mv CNAME /root/
 fi
 
-cd $PUBLISH_DIR
-
-echo "Config git ..."
-
-# Configures Git.
-git init
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git remote add origin "${REPOSITORY_PATH}"
-
-# Checks to see if the remote exists prior to deploying.
-# If the branch doesn't exist it gets created here as an orphan.
-# if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$BRANCH" | wc -l)" -eq 0 ];
-# then
-#   echo "Creating remote branch ${BRANCH} as it doesn't exist..."
-#   git checkout --orphan $BRANCH
-# fi
-
-git checkout --orphan $BRANCH
+git rm -r '*'
+cp -r /root/public/. .
+if [ -n "/root/CNAME" ]; then
+  mv CNAME .
+fi
 
 git add --all
 
@@ -76,6 +50,6 @@ echo 'Start Commit'
 git commit --allow-empty -m "Deploying to ${BRANCH}"
 
 echo 'Start Push'
-git push origin "${BRANCH}" --force
+git push origin "${BRANCH}"
 
 echo "Deployment succesful!"
